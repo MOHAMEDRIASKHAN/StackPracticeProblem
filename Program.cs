a79af9e96767fb7c03dc6891119daaa8ef0ba4d9
MOHAMEDRIASKHAN <mohamedriaskhan@gmail.com>
2022-08-25 20:05:23 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+namespace StackPracticeProblem
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Stack<string> stack = new Stack<string>();
+            stack.Push("56");
+            stack.Push("30");
+            stack.Push("70");
+
+            foreach(string ele in stack)
+            {
+                Console.WriteLine(ele);
+            }
+
+            stack.Pop();
+
+            Console.WriteLine("The peek element is " +stack.Peek());
+            Console.WriteLine("--------------------");
+
+                foreach (string ele in stack)
+            {
+                Console.WriteLine(ele);
+            }
+        }
+    }
+}
